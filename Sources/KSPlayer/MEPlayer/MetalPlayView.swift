db02c1329a30a27d91eb5867120d1f194bff9c3d
Ronnie W. <llqoli@gmail.com>
2024-05-26 15:38:17 +0800

    Update MetalPlayView.swift

--- Sources/KSPlayer/MEPlayer/MetalPlayView.swift
+++ Sources/KSPlayer/MEPlayer/MetalPlayView.swift
@@ -9,7 +9,7 @@ import AVFoundation
 import Combine
 import CoreMedia
 #if canImport(MetalKit)
-import MetalKit
+    import MetalKit
 #endif
 public protocol DisplayLayerDelegate: NSObjectProtocol {
     func change(displayLayer: AVSampleBufferDisplayLayer)
@@ -124,21 +124,21 @@ public final class MetalPlayView: UIView, VideoOutput {
     }
 
     #if canImport(UIKit)
-    override public func touchesMoved(_ touches: Set<UITouch>, with: UIEvent?) {
-        if options.display == .plane {
-            super.touchesMoved(touches, with: with)
-        } else {
-            options.display.touchesMoved(touch: touches.first!)
+        override public func touchesMoved(_ touches: Set<UITouch>, with: UIEvent?) {
+            if options.display == .plane {
+                super.touchesMoved(touches, with: with)
+            } else {
+                options.display.touchesMoved(touch: touches.first!)
+            }
         }
-    }
     #else
-    override public func touchesMoved(with event: NSEvent) {
-        if options.display == .plane {
-            super.touchesMoved(with: event)
-        } else {
-            options.display.touchesMoved(touch: event.allTouches().first!)
+        override public func touchesMoved(with event: NSEvent) {
+            if options.display == .plane {
+                super.touchesMoved(with: event)
+            } else {
+                options.display.touchesMoved(touch: event.allTouches().first!)
+            }
         }
-    }
     #endif
 
     public func flush() {
@@ -242,7 +242,7 @@ extension MetalPlayView {
 class MetalView: UIView {
     private let render = MetalRender()
     #if canImport(UIKit)
-    override public class var layerClass: AnyClass { CAMetalLayer.self }
+        override public class var layerClass: AnyClass { CAMetalLayer.self }
     #endif
     var metalLayer: CAMetalLayer {
         // swiftlint:disable force_cast
@@ -253,7 +253,7 @@ class MetalView: UIView {
     init() {
         super.init(frame: .zero)
         #if !canImport(UIKit)
-        layer = CAMetalLayer()
+            layer = CAMetalLayer()
         #endif
         metalLayer.device = MetalRender.device
         metalLayer.framebufferOnly = true
@@ -278,19 +278,20 @@ class MetalView: UIView {
         if colorspace != nil, metalLayer.colorspace != colorspace {
             metalLayer.colorspace = colorspace
             KSLog("[video] CAMetalLayer colorspace \(String(describing: colorspace))")
+
             #if !os(tvOS)
-            if #available(iOS 16.0, *) {
-                if let name = colorspace?.name, name != CGColorSpace.sRGB {
-                    #if os(macOS)
-                    metalLayer.wantsExtendedDynamicRangeContent = window?.screen?.maximumPotentialExtendedDynamicRangeColorComponentValue ?? 1.0 > 1.0
-                    #else
-                    metalLayer.wantsExtendedDynamicRangeContent = true
-                    #endif
-                } else {
-                    metalLayer.wantsExtendedDynamicRangeContent = false
+                if #available(iOS 16.0, *) {
+                    if let name = colorspace?.name, name != CGColorSpace.sRGB {
+                        #if os(macOS)
+                            metalLayer.wantsExtendedDynamicRangeContent = window?.screen?.maximumPotentialExtendedDynamicRangeColorComponentValue ?? 1.0 > 1.0
+                        #else
+                            metalLayer.wantsExtendedDynamicRangeContent = true
+                        #endif
+                    } else {
+                        metalLayer.wantsExtendedDynamicRangeContent = false
+                    }
+                    KSLog("[video] CAMetalLayer wantsExtendedDynamicRangeContent \(metalLayer.wantsExtendedDynamicRangeContent)")
                 }
-                KSLog("[video] CAMetalLayer wantsExtendedDynamicRangeContent \(metalLayer.wantsExtendedDynamicRangeContent)")
-            }
             #endif
         }
         guard let drawable = metalLayer.nextDrawable() else {
@@ -303,7 +304,7 @@ class MetalView: UIView {
 
 class AVSampleBufferDisplayView: UIView {
     #if canImport(UIKit)
-    override public class var layerClass: AnyClass { AVSampleBufferDisplayLayer.self }
+        override public class var layerClass: AnyClass { AVSampleBufferDisplayLayer.self }
     #endif
     var displayLayer: AVSampleBufferDisplayLayer {
         // swiftlint:disable force_cast
@@ -314,7 +315,7 @@ class AVSampleBufferDisplayView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         #if !canImport(UIKit)
-        layer = AVSampleBufferDisplayLayer()
+            layer = AVSampleBufferDisplayLayer()
         #endif
         var controlTimebase: CMTimebase?
         CMTimebaseCreateWithSourceClock(allocator: kCFAllocatorDefault, sourceClock: CMClockGetHostTimeClock(), timebaseOut: &controlTimebase)
@@ -363,84 +364,84 @@ class AVSampleBufferDisplayView: UIView {
 }
 
 #if os(macOS)
-import CoreVideo
+    import CoreVideo
 
-class CADisplayLink {
-    private let displayLink: CVDisplayLink
-    private var runloop: RunLoop?
-    private var mode = RunLoop.Mode.default
-    public var preferredFramesPerSecond = 60
-    @available(macOS 12.0, *)
-    public var preferredFrameRateRange: CAFrameRateRange {
-        get {
-            CAFrameRateRange()
+    class CADisplayLink {
+        private let displayLink: CVDisplayLink
+        private var runloop: RunLoop?
+        private var mode = RunLoop.Mode.default
+        public var preferredFramesPerSecond = 60
+        @available(macOS 12.0, *)
+        public var preferredFrameRateRange: CAFrameRateRange {
+            get {
+                CAFrameRateRange()
+            }
+            set {}
         }
-        set {}
-    }
 
-    public var timestamp: TimeInterval {
-        var timeStamp = CVTimeStamp()
-        if CVDisplayLinkGetCurrentTime(displayLink, &timeStamp) == kCVReturnSuccess, (timeStamp.flags & CVTimeStampFlags.hostTimeValid.rawValue) != 0 {
-            return TimeInterval(timeStamp.hostTime / NSEC_PER_SEC)
+        public var timestamp: TimeInterval {
+            var timeStamp = CVTimeStamp()
+            if CVDisplayLinkGetCurrentTime(displayLink, &timeStamp) == kCVReturnSuccess, (timeStamp.flags & CVTimeStampFlags.hostTimeValid.rawValue) != 0 {
+                return TimeInterval(timeStamp.hostTime / NSEC_PER_SEC)
+            }
+            return 0
         }
-        return 0
-    }
 
-    public var duration: TimeInterval {
-        CVDisplayLinkGetActualOutputVideoRefreshPeriod(displayLink)
-    }
-
-    public var targetTimestamp: TimeInterval {
-        duration + timestamp
-    }
-
-    public var isPaused: Bool {
-        get {
-            !CVDisplayLinkIsRunning(displayLink)
+        public var duration: TimeInterval {
+            CVDisplayLinkGetActualOutputVideoRefreshPeriod(displayLink)
         }
-        set {
-            if newValue {
-                CVDisplayLinkStop(displayLink)
-            } else {
-                CVDisplayLinkStart(displayLink)
+
+        public var targetTimestamp: TimeInterval {
+            duration + timestamp
+        }
+
+        public var isPaused: Bool {
+            get {
+                !CVDisplayLinkIsRunning(displayLink)
+            }
+            set {
+                if newValue {
+                    CVDisplayLinkStop(displayLink)
+                } else {
+                    CVDisplayLinkStart(displayLink)
+                }
+            }
+        }
+
+        public init(target: NSObject, selector: Selector) {
+            var displayLink: CVDisplayLink?
+            CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+            self.displayLink = displayLink!
+            CVDisplayLinkSetOutputHandler(self.displayLink) { [weak self] _, _, _, _, _ in
+                guard let self else { return kCVReturnSuccess }
+                self.runloop?.perform(selector, target: target, argument: self, order: 0, modes: [self.mode])
+                return kCVReturnSuccess
+            }
+            CVDisplayLinkStart(self.displayLink)
+        }
+
+        public init(block: @escaping (() -> Void)) {
+            var displayLink: CVDisplayLink?
+            CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+            self.displayLink = displayLink!
+            CVDisplayLinkSetOutputHandler(self.displayLink) { _, _, _, _, _ in
+                block()
+                return kCVReturnSuccess
+            }
+            CVDisplayLinkStart(self.displayLink)
+        }
+
+        open func add(to runloop: RunLoop, forMode mode: RunLoop.Mode) {
+            self.runloop = runloop
+            self.mode = mode
+        }
+
+        public func invalidate() {
+            isPaused = true
+            runloop = nil
+            CVDisplayLinkSetOutputHandler(displayLink) { _, _, _, _, _ in
+                kCVReturnError
             }
         }
     }
-
-    public init(target: NSObject, selector: Selector) {
-        var displayLink: CVDisplayLink?
-        CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
-        self.displayLink = displayLink!
-        CVDisplayLinkSetOutputHandler(self.displayLink) { [weak self] _, _, _, _, _ in
-            guard let self else { return kCVReturnSuccess }
-            self.runloop?.perform(selector, target: target, argument: self, order: 0, modes: [self.mode])
-            return kCVReturnSuccess
-        }
-        CVDisplayLinkStart(self.displayLink)
-    }
-
-    public init(block: @escaping (() -> Void)) {
-        var displayLink: CVDisplayLink?
-        CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
-        self.displayLink = displayLink!
-        CVDisplayLinkSetOutputHandler(self.displayLink) { _, _, _, _, _ in
-            block()
-            return kCVReturnSuccess
-        }
-        CVDisplayLinkStart(self.displayLink)
-    }
-
-    open func add(to runloop: RunLoop, forMode mode: RunLoop.Mode) {
-        self.runloop = runloop
-        self.mode = mode
-    }
-
-    public func invalidate() {
-        isPaused = true
-        runloop = nil
-        CVDisplayLinkSetOutputHandler(displayLink) { _, _, _, _, _ in
-            kCVReturnError
-        }
-    }
-}
 #endif
